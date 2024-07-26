b32319f87270582a1aaa59a70e95fcc17616c00b
manavnotop <manuagarwal1443@gmail.com>
2024-07-26 15:20:54 +0530

    add user schema

--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,31 @@ import mongoose from "mongoose";
 
 mongoose.connect("mongodb+srv://manavnotop:%3Cpassword%3E@cluster0.bu6qrds.mongodb.net/todo-api");
 
+const userSchema = mongoose.Schema({
+    username:{
+        required: true,
+        type: String,
+    },
+    password:{
+        require: true,
+        type: String,
+    },
+    firstName: {
+        require: true,
+        type: String,
+    },
+    lastName: {
+        require: true,
+        type: String
+    }
+})
+
 const todoSchema = mongoose.Schema({
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
     title:{
         required: true,
         type: String,
@@ -16,8 +40,10 @@ const todoSchema = mongoose.Schema({
     }
 })
 
-const Todo = mongoose.model(Todo, todoSchema);
+const User = mongoose.model('User', userSchema);
+const Todo = mongoose.model('Todo', todoSchema);
 
 module.exports = {
     Todo,
+    User,
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import express from "express";
 
-const app = express()
+const app = express();
 
 app.listen(300);
